56a0967ed3e151273f42e15638c4904590f88cca
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-04-06 00:03:52 +0300

    Add Rating component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { Htag, Button, P, Tag } from '../components';
-
+import React, { useEffect, useState } from 'react';
+import { Htag, Button, P, Tag, Rating } from '../components';
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
-      <Htag tag='h1'>Текст</Htag>
-      <Button appearance={'primary'} arrow='right'>Кнопка</Button>
-      <Button appearance={'ghost'} arrow='down'>Кнопка</Button>
+      <Htag tag='h1'>Заголовок</Htag>
+      <Button appearance={'primary'} arrow='right'>
+        Кнопка
+      </Button>
+      <Button appearance={'ghost'} arrow='down'>
+        Кнопка
+      </Button>
       <P size='l'>Большой</P>
       <P>Средний</P>
       <P size='s'>Маленький</P>
-      <Tag size='s' color='ghost'>Ghost</Tag>
-      <Tag size='s' color='red'>Red</Tag>
-      <Tag size='m' color='green'>Green</Tag>
-      <Tag size='m' color='grey'>Grey</Tag>
+      <Tag size='s' color='ghost'>
+        Ghost
+      </Tag>
+      <Tag size='s' color='red'>
+        Red
+      </Tag>
+      <Tag size='m' color='green'>
+        Green
+      </Tag>
+      <Tag size='m' color='grey'>
+        Grey
+      </Tag>
       <Tag color='primary'>Primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
     </>
   );
 }
