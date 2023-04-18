43e85656d743a80821b816998f87aa436342bd55
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-04-18 11:11:11 +0300

    refactoring

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -6,7 +6,7 @@ import { FirstLevelMenuItem } from '../interfaces/menu.interface';
 import { TopLevelCategory } from '../interfaces/page.interface';
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
-  { route: '[type]', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
   { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
   { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
   { route: 'products', name: 'Товары', icon: <ProductsIcon />, id: TopLevelCategory.Products },
--- next.config.js
+++ next.config.js
@@ -1,25 +1,15 @@
 module.exports = {
   images: {
-    domains: ['[type]-top.ru'],
+    domains: ['courses-top.ru']
   },
   webpack(config, options) {
     config.module.rules.push({
       loader: '@svgr/webpack',
-      issuer: /\.[jt]sx?$/,
       options: {
         prettier: false,
         svgo: true,
         svgoConfig: {
-          plugins: [
-            {
-              name: 'preset-default',
-              params: {
-                override: {
-                  removeViewBox: false,
-                },
-              },
-            },
-          ],
+          plugins: [{ removeViewBox: false }],
         },
         titleProp: true,
       },
